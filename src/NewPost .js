fb03b8db5ddba5d690b9691d5978d571139a9624
yuli <yulimalin22@gmail.com>
2021-06-06 11:58:11 +0300

    06.06.21

---
+++ src/NewPost .js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./header";
+
+function NewPosts() {
+  return (
+    <div>
+      <h1>new post page</h1>
+      <p>this page is to create new post</p>
+    </div>
+  );
+}
+
+export default NewPosts;
---
+++ src/Postpage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Posts from "./posts";
+import { useParams } from "react-router-dom";
+
+function Postpage() {
+  let { id } = useParams();
+  return (
+    <div>
+      <h1>{Posts.posts[id - 1].title}</h1>
+      <p>{Posts.posts[id - 1].content}</p>
+    </div>
+  );
+}
+
+export default Postpage;
